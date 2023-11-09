28572b22a240cf1913278737d825261eee88feb1
huowenjie <huowenjie0428@sina.com>
2023-11-09 15:59:49 +0800

    修改注释

--- GraphicRender/gr_filter.h
+++ GraphicRender/gr_filter.h
@@ -21,7 +21,7 @@ GR_FILTER *gr_filter_create(GR_FILTER_TYPE type);
 /* 销毁滤波器 */
 void gr_filter_destroy(GR_FILTER *filter);
 
-/* 获取滤波器值，index 范围为 [0, 2r] 2r + 1 个值 */
+/* 获取滤波器值，index 范围为 [-r, r] 2r + 1 个值 */
 float gr_filter_get(const GR_FILTER *filter, int i, int j);
 
 /* 获取过滤半径 */
