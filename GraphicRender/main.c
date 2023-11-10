52dafdbd6ed60a38ead59c49e1edcd48f7c17920
huowenjie <huowenjie0428@sina.com>
2023-11-10 15:18:35 +0800

    添加矩阵操作

--- GraphicRender/main.c
+++ GraphicRender/main.c
@@ -6,31 +6,11 @@
 
 /*---------------------------------------------------------------------------*/
 
-void draw_circle(GR_ENGINE *engine, const GR_VEC2F *p, float r, GR_RGB rgb)
-{
-    float d = r + 10.0f;
-    float i = p->x - d;
-    float j = 0.0f;
-
-    for (; i < (p->x + d); i += 1.0f) {
-        for (j = p->y - d; j < (p->y + d); j += 1.0f) {
-            GR_VEC2F pt = { i, j };
-            GR_VEC2F v = gr_vec2f_sub(&pt, p);
-
-            if (gr_vec2f_length(&v) <= r) {
-                gr_engine_draw_pixel(engine, (int)pt.x, (int)pt.y, 0xFFFF00FF);
-            }
-        }
-    }
-}
-
 GR_UINT32 render_update(GR_ENGINE *engine, void *args)
 {
     GR_VEC2F pt = { 0.0f, 0.0f };
 
-    draw_circle(engine, &pt, 100.0f, 0x00FF00FF);
-    //gr_engine_draw_pixel(engine, (int)pt.x, (int)pt.y, 0x00FF00FF);
-    gr_engine_draw_line(engine, 40, 0, 50, 359, 0xFFFF00FF);
+    gr_engine_draw_line(engine, 0, 0, 479, 359, 0xFFFFFFFF);
     return 0;
 }
 
@@ -42,7 +22,6 @@ int main(int argc, char *argv[])
     GR_MEM_START;
 
     engine = gr_engine_create(480, 360);
-    //engine = gr_engine_create(100, 100);
     gr_engine_set_update(engine, render_update, NULL);
     gr_engine_render(engine);
     gr_engine_destroy(engine);
